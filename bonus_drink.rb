6cacf378793706646d3ae041d04a2718a8567442
tamaxyo <tamaxyo@gmail.com>
2017-03-24 00:59:46 +0900

    implement bonus drink counter

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_count = amount
+    empty_bottles = amount
+
+    while empty_bottles > 2
+      bonus_drink = empty_bottles / 3
+
+      total_count += bonus_drink
+      empty_bottles = bonus_drink + empty_bottles % 3
+    end
+
+    return total_count
   end
 end
